dd3b5925ee3e45e21ae19b784617ef325708b293
Yuli Yang <yuli@figmints.com>
2013-08-17 22:19:43 -0400

    new page layout

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,82 +1,100 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en" >
-    <head>
-        <!-- Basic Page Needs
-        ================================================== -->
-        <meta charset="utf-8">
-        <title>Proving Grounds</title>
-        <meta name="description" content="Proving">
-        <!-- Mobile Specific Metas
-        ================================================== -->
-        <meta name="viewport" content="width=device-width">
-        <!-- CSS
-        ================================================== -->
-        <link rel="stylesheet" href="<?= BASE_URL?>/public/css/app.css">
-        <link rel="stylesheet" href="<?= BASE_URL?>/public/css/main.css">
-        <!-- JS
-        ================================================== -->
-        <script src="<?= BASE_URL?>/public/js/vendor/custom.modernizr.js"></script>
-        <script language="JavaScript" type="text/JavaScript" src="<?= BASE_URL?>/public/js/jquery-1.7.2.min.js"></script>
-        <script language="JavaScript" type="text/JavaScript" src="<?= BASE_URL?>/public/js/homepage.js"></script>
-
-        <!-- Favicons
-        ================================================== -->
-        <link rel="shortcut icon" href="<?= BASE_URL?>/public/images/favicon.ico">
-        <link rel="apple-touch-icon" href="<?= BASE_URL?>/public/images/apple-touch-icon.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="<?= BASE_URL?>/public/images/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="<?= BASE_URL?>/public/images/apple-touch-icon-114x114.png">
-    </head>
-    <body>
-        <header class="topNav">
-            <div class="row">
-                <div class="large-7 columns"><img src="" alt="">logo</div>
-                <nav class="large-5 columns">
-                    <ul class="large-block-grid-4">
-                        <li><a href="">About</a></li>
-                        <li><a href="">Sign-in</a></li>
-                        <li><a href="">Forum</a></li>
-                        <li><a href="">Search</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-        <section class="">
-
-        </section>
-        <footer>
-            <div class="row">
-                <nav class="large-5 columns">
-                    <ul class="large-block-grid-4">
-                        <li><a href="">About</a></li>
-                        <li><a href="">Sign-in</a></li>
-                        <li><a href="">Forum</a></li>
-                        <li><a href="">Search</a></li>
-                    </ul>
-                </nav>
-                <div class="large-2 columns">
-                    © IdeaPuff, Inc. 2013.
-                </div>
-                <div class="large-5 columns">
-                    social icons
-                </div>
-            </div>
-        </footer>
-        <script>
-        document.write('<script src=' +
-        ('__proto__' in {} ? 'public/js/vendor/zepto' : 'public/js/vendor/jquery') +
-        '.js><\/script>')
-        </script>
-        <script src="public/js/foundation/foundation.js"></script>
-        <script src="public/js/foundation/foundation.abide.js"></script>
-        <script src="public/js/foundation/foundation.forms.js"></script>
-        <script src="public/js/foundation/foundation.joyride.js"></script>
-        <script src="public/js/foundation/foundation.magellan.js"></script>
-        <script src="public/js/foundation/foundation.orbit.js"></script>
-        <script src="public/js/foundation/foundation.placeholder.js"></script>
-        <script src="public/js/foundation/foundation.section.js"></script>
-        <script src="public/js/foundation/foundation.tooltips.js"></script>
-        <script>
-        $(document).foundation();
-        </script>
-    </body>
+	<head>
+		<!-- Basic Page Needs
+		================================================== -->
+		<meta charset="utf-8">
+		<title>Proving Grounds</title>
+		<meta name="description" content="Proving">
+		<!-- Mobile Specific Metas
+		================================================== -->
+		<meta name="viewport" content="width=device-width">
+		<!-- CSS
+		================================================== -->
+		<link rel="stylesheet" href="<?= BASE_URL?>/public/css/app.css">
+		<link rel="stylesheet" href="<?= BASE_URL?>/public/css/main.css">
+		<link rel="stylesheet" href="<?= BASE_URL?>/public/css/dashboard.css">
+		<!-- JS
+		================================================== -->
+		<script src="<?= BASE_URL?>/public/js/vendor/custom.modernizr.js"></script>
+		<script language="JavaScript" type="text/JavaScript" src="<?= BASE_URL?>/public/js/jquery-1.7.2.min.js"></script>
+		<script language="JavaScript" type="text/JavaScript" src="<?= BASE_URL?>/public/js/homepage.js"></script>
+		<!-- Favicons
+		================================================== -->
+		<link rel="shortcut icon" href="<?= BASE_URL?>/public/images/favicon.ico">
+		<link rel="apple-touch-icon" href="<?= BASE_URL?>/public/images/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="<?= BASE_URL?>/public/images/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="<?= BASE_URL?>/public/images/apple-touch-icon-114x114.png">
+	</head>
+	<body>
+		<header class="topNav">
+			<div class="row">
+				<div class="large-7 columns"><img src="" alt="">logo</div>
+				<nav class="large-5 columns">
+					<ul class="large-block-grid-4">
+						<li><a href="">About</a></li>
+						<li><a href="">Sign-in</a></li>
+						<li><a href="">Forum</a></li>
+						<li><a href="">Search</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<section class="mainContent">
+			<div class="row">
+				<div class="large-4 columns">
+					<div class="userPhoto">
+						<img src="userImageURL" alt="">
+						<strong>UserName</strong><small>UserLevel</small>
+					</div>
+					<ul>
+						<li><a href="">Sub nav</a></li>
+						<li><a href="">Sub nav</a></li>
+						<li><a href="">Sub nav</a></li>
+					</ul>
+				</div>
+				<div class="large-8 columns">
+					<section class="activityFeed">
+						<h4>Activity feed title (Example: Tim joined my team Proving Grounds.)</h4>
+						<small>Activity Time (Example: 5 secs ago)</small>
+					</section>
+				</div>
+			</div>
+		</section>
+		<footer>
+			<div class="row">
+				<nav class="large-5 columns">
+					<ul class="large-block-grid-4">
+						<li><a href="">About</a></li>
+						<li><a href="">Sign-in</a></li>
+						<li><a href="">Forum</a></li>
+						<li><a href="">Search</a></li>
+					</ul>
+				</nav>
+				<div class="large-2 columns">
+					© IdeaPuff, Inc. 2013.
+				</div>
+				<div class="large-5 columns">
+					social icons
+				</div>
+			</div>
+		</footer>
+		<script>
+		document.write('<script src=' +
+		('__proto__' in {} ? 'public/js/vendor/zepto' : 'public/js/vendor/jquery') +
+		'.js><\/script>')
+		</script>
+		<script src="public/js/foundation/foundation.js"></script>
+		<script src="public/js/foundation/foundation.abide.js"></script>
+		<script src="public/js/foundation/foundation.forms.js"></script>
+		<script src="public/js/foundation/foundation.joyride.js"></script>
+		<script src="public/js/foundation/foundation.magellan.js"></script>
+		<script src="public/js/foundation/foundation.orbit.js"></script>
+		<script src="public/js/foundation/foundation.placeholder.js"></script>
+		<script src="public/js/foundation/foundation.section.js"></script>
+		<script src="public/js/foundation/foundation.tooltips.js"></script>
+		<script>
+		$(document).foundation();
+		</script>
+	</body>
 </html>
