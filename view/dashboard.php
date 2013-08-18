228337a57a29ca3b9fc76ae7278dcbdba80205ff
Yuli Yang <yuli@figmints.com>
2013-08-18 12:22:41 -0400

    activity feed

--- view/dashboard.php
+++ view/dashboard.php
@@ -14,13 +14,38 @@ include 'includes/header.php';
         <div class="large-8 columns mainContainer">
             <section class="activityInviteFriends">
                 <p>Find Out Who Else is Using Proving Grounds</p>
-                <p>Subtitle</p>
-                <input type="email" placeholder="your@email.com">
+                <!-- <p>Subtitle</p> -->
+                <input type="email" placeholder="friends@email.com">
                 <input type="submit" value="Continue" class="btnWhite">
             </section>
             <section class="activityFeed">
-                <p>Activity feed title (Example: Tim joined my team <a href="">Proving Grounds</a>.)</p>
-                <p>Activity Time (Example: 5 secs ago)</p>
+                <p class="left">Finckys.Marque has just created a team.</p>
+                <p class="right">5 secs ago</p>
+                <br class="clear">
+                <p class="left">Finckys.Marque has just created a team.</p>
+                <p class="right">5 secs ago</p>
+                <br class="clear">
+                <p class="left">Dyrus.Liliana Dose any team looking for a Top player? My style is aggressive.</p>
+                <p class="right">5 secs ago</p>
+                <br class="clear">
+                <p class="left">Yueling I am usually play a support, and looking to joy some team, where needed a support role and play often.</p>
+                <p class="right">5 secs ago</p>
+                <br class="clear">
+                <p class="left">Galaxywar inviting you to join their team.</p>
+                <p class="right">5 secs ago</p>
+                <br class="clear">
+                <p class="left">Boppppppp starting to follow you.</p>
+                <p class="right">5 secs ago</p>
+                <br class="clear">
+                <p class="left">wentorr added to your summoner list.</p>
+                <p class="right">5 secs ago</p>
+                <br class="clear">
+                <p class="left">launching champion looking for another AD player, to replace our current main AD, who will stop the frequency of playing for a while.</p>
+                <p class="right">5 secs ago</p>
+                <br class="clear">
+                <p class="left">Boppppppp starting to follow you.</p>
+                <p class="right">5 secs ago</p>
+                <br class="clear">
             </section>
         </div>
     </div>
