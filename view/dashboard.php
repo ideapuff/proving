c6aa2ee82a87a0fe4109e23727ecd4ae72c8877f
Yuli Yang <yuli@figmints.com>
2013-08-17 20:01:05 -0400

    change image

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,16 +1,146 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
 <!DOCTYPE html>
-<html>
+<html class="no-js" lang="en" >
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <!-- Basic Page Needs
+        ================================================== -->
+        <meta charset="utf-8">
+        <title>Proving Grounds</title>
+        <meta name="description" content="Proving">
+        <!-- Mobile Specific Metas
+        ================================================== -->
+        <meta name="viewport" content="width=device-width">
+        <!-- CSS
+        ================================================== -->
+        <link rel="stylesheet" href="<?= BASE_URL?>/public/css/app.css">
+        <link rel="stylesheet" href="<?= BASE_URL?>/public/css/main.css">
+        <!-- JS
+        ================================================== -->
+        <script src="<?= BASE_URL?>/public/js/vendor/custom.modernizr.js"></script>
+        <script language="JavaScript" type="text/JavaScript" src="<?= BASE_URL?>/public/js/jquery-1.7.2.min.js"></script>
+        <script language="JavaScript" type="text/JavaScript" src="<?= BASE_URL?>/public/js/homepage.js"></script>
+
+        <!-- Favicons
+        ================================================== -->
+        <link rel="shortcut icon" href="<?= BASE_URL?>/public/images/favicon.ico">
+        <link rel="apple-touch-icon" href="<?= BASE_URL?>/public/images/apple-touch-icon.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= BASE_URL?>/public/images/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= BASE_URL?>/public/images/apple-touch-icon-114x114.png">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <section class="homeTop">
+            <div class="row">
+                <div class="large-10 small-centered columns">
+                    <img class="logo" src="<?= BASE_URL?>/public/images/logo.png" alt="">
+                    <h1><span>Prove it.</span> Be the best you can be.</h1>
+                    <h3>Nam ut magna risus. Cras vestibulum sagittis lacinia. Pellentesque et cursus velit, id pretium enim. Fusce vel mollis tortor. </h3>
+                    <div class="actionBtn">
+                        <a href="#learn-more" class="btnWhite">Learn More</a>
+                        <a href="#get-started" class="btnWhite">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <header class="topNav">
+            <div class="row">
+                <div class="large-7 columns"><img src="" alt="">logo</div>
+                <nav class="large-5 columns">
+                    <ul class="large-block-grid-4">
+                        <li><a href="">About</a></li>
+                        <li><a href="">Sign-in</a></li>
+                        <li><a href="">Forum</a></li>
+                        <li><a href="">Search</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <section class="homeMain">
+            <div class="row">
+                <div class="large-6 columns homeMainLeft">
+                    <a name="learn-more"></a>
+                    <h2 class="first-child">It's all about <span>YOU</span>.</h2>
+                    <p>We understand gaming is your profession. <br/>
+                    You deserve a professional proﬁle too.</p>
+                    <ul>
+                        <li>customizable proﬁle page</li>
+                        <li>inbox</li>
+                        <li>freind list</li>
+                    </ul>
+                    <h2>Get the right <span>TEAMMATES</span>.</h2>
+                    <p>You will get a professional proﬁle page. We understand gaming is your profession. You deserve a professional proﬁle too. </p>
+                    <ul>
+                        <li>Team page &amp; communication</li>
+                        <li>community</li>
+                        <li>freind list</li>
+                    </ul>
+                    <h2>It’s all realtime <span>ACTIVITY FEED</span>.</h2>
+                    <p>Simple, clear and fast. That’s all you want, and we get it for you.</p>
+                    <a href="">Sign-in</a>
+                </div>
+                <div class="large-6 columns homeMainRight">
+                    <h2>Powerful FILTER SEARCH.</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nemo, numquam, natus, ab quos ipsa consectetur quas eos placeat ad blanditiis provident dignissimos atque error eaque quisquam ullam. Similique, atque.</p>
+                    <h2>It's all about YOU.</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nemo, numquam, natus, ab quos ipsa consectetur quas eos placeat ad blanditiis provident dignissimos atque error eaque quisquam ullam. Similique, atque.</p>
+                    <h2>It's all about YOU.</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nemo, numquam, natus, ab quos ipsa consectetur quas eos placeat ad blanditiis provident dignissimos atque error eaque quisquam ullam. Similique, atque.</p>
+                    <a href="">Get Started</a>
+                </div>
+            </div>
+        </section>
+        <section class="homeWidget">
+            <div class="row">
+                <div class="large-4 columns">
+                    <h4>Latest News</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, vero, ratione saepe delectus beatae dolorum consequatur maxime explicabo accusantium asperiores perspiciatis molestias aut praesentium. Architecto corrupti eligendi dolorem consectetur itaque.</p>
+                    <small>Aug. 18. 2013</small>
+                </div>
+                <div class="large-8 columns">
+                    <h4>Check Out New Members</h4>
+                </div>
+            </div>
+        </section>
+        <section class="homeAbout">
+            <div class="row">
+                <div class="large-12 columns">
+                    <h2>About Proving Grounds</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, atque minus architecto ex repellendus culpa magnam ab unde necessitatibus error dignissimos quis ipsum tempora et obcaecati tenetur magni debitis voluptatum?</p>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="row">
+                <nav class="large-5 columns">
+                    <ul class="large-block-grid-4">
+                        <li><a href="">About</a></li>
+                        <li><a href="">Sign-in</a></li>
+                        <li><a href="">Forum</a></li>
+                        <li><a href="">Search</a></li>
+                    </ul>
+                </nav>
+                <div class="large-2 columns">
+                    © IdeaPuff, Inc. 2013.
+                </div>
+                <div class="large-5 columns">
+                    social icons
+                </div>
+            </div>
+        </footer>
+        <script>
+        document.write('<script src=' +
+        ('__proto__' in {} ? 'public/js/vendor/zepto' : 'public/js/vendor/jquery') +
+        '.js><\/script>')
+        </script>
+        <script src="public/js/foundation/foundation.js"></script>
+        <script src="public/js/foundation/foundation.abide.js"></script>
+        <script src="public/js/foundation/foundation.forms.js"></script>
+        <script src="public/js/foundation/foundation.joyride.js"></script>
+        <script src="public/js/foundation/foundation.magellan.js"></script>
+        <script src="public/js/foundation/foundation.orbit.js"></script>
+        <script src="public/js/foundation/foundation.placeholder.js"></script>
+        <script src="public/js/foundation/foundation.section.js"></script>
+        <script src="public/js/foundation/foundation.tooltips.js"></script>
+        <script>
+        $(document).foundation();
+        </script>
     </body>
 </html>
